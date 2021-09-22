5c804f115bd1fc5498a8a5cf51fb14143a54cbe8
yugyeongh <uugyeong27@gmail.com>
2021-09-22 23:25:49 +0900

    week02 - add config

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const {User} = require('./models/User');
 
+const config = require('./config/key');
+
+// bodyParser가 client에서 오는 정보를 서버에서 분석해서 가져올 수 있게 하는 것
+// applocation/x-www-form-urlencoded를 분석해서 가져오는 것
+app.use(bodyParser.urlencoded({entended: true}));
+// application/json
+app.use(bodyParser.json());
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hyeon:abcd1234@nodejs.gohxx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     /* 오류가 생길 수 있어 적어주는 코드 */
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
@@ -14,7 +23,29 @@ mongoose.connect('mongodb+srv://hyeon:abcd1234@nodejs.gohxx.mongodb.net/myFirstD
   .then(()=>console.log('MongoDB Connected...'))
   .catch((err)=>console.log(err));
 
-//
+app.get('/',(req,res)=>res.send('후훗'));
 
-app.get('/',(req,res)=>res.send('Hello World!'))
-app.listen(port,()=>console.log(`${port}번 포트 연결했다!`))
+//login 라우터
+app.post('/register',(req,res)=>{
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 DB에 넣어준다.
+
+  /*
+  json 형식으로 user 정보가  req.body로 전달됨
+  {
+    id:'hello',
+    pw:'aaa',
+  }
+  */
+ // json 형식을 객체로 만들기
+  const user = new User(req.body);
+
+  user.save((err,doc)=>{
+    if (err) return res.json ({success: false, err});
+    return res.status(200).json({
+      success: true,
+    })
+  })
+})
+
+app.listen(port,()=>console.log(`${port}번 포트 연결했다!`));
